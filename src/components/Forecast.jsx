61568d18057bdf34f50b2c060d848b191f3e07bc
Nkosinathi Mafuleka <107852753+nathimafuleka@users.noreply.github.com>
2023-05-03 12:14:09 +0200

    Update Forecast.jsx

--- src/components/Forecast.jsx
+++ src/components/Forecast.jsx
@@ -1,53 +1,24 @@
 import React from 'react'
 import "./forecast.css"
+import moment from "moment";
 
-import { FaWind } from "react-icons/fa";
-import { BiHappy } from "react-icons/bi";
-import { MdCompress, MdOutlineWaterDrop } from "react-icons/md";
 
-const Forecast = ({ weather }) => {
-    const tempUnit  = "metric" ? "°C" : "°F";
-    const windUnit  = "metric" ? "m/s" : "m/h";
-  
-    const cards = [
-      {
-        id: 1,
-        icon: <BiHappy />,
-        title: "feels like",
-        data: weather.feels_like.toFixed(),
-        unit: tempUnit,
-      },
-      {
-        id: 2,
-        icon: <MdCompress />,
-        title: "pressure",
-        data: weather.pressure,
-        unit: "hPa",
-      },
-      {
-        id: 3,
-        icon: <MdOutlineWaterDrop />,
-        title: "humidity",
-        data: weather.humidity,
-        unit: "%",
-      },
-      {
-        id: 4,
-        icon: <FaWind />,
-        title: "wind speed",
-        data: weather.speed.toFixed(),
-        unit: windUnit,
-      },
-    ];
+
+const Forecast = ({ forecast }) => {
+
+  const tempUnit  = "metric" ? "°C" : "°F";
+
     return (
       <div className="section section__forecast">
-        {cards.map(({ id, icon, title, data, unit }) => (
-          <div key={id} className="card">
+        {forecast.map((data) => (
+          <div className="card">
             <div className="forecast__card-icon">
-              {icon}
-              <small>{title}</small>
+            <img src={data.iconURL} alt="weatherIcon" />
             </div>
-            <h2>{`${data} ${unit}`}</h2>
+            <small>{moment(data.dt_txt).format('dddd')}</small>
+            <br />
+            <h2>{`${data.temp.toFixed()} ${tempUnit}`}</h2>
+            <small>{data.description}</small>
           </div>
         ))}
       </div>
